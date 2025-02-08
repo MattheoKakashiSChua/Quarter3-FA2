315e0d3a13675261cba51c3abd2c1622fa5cc98b
MattheoKakashiSChua <mattheokakashi.chua@smc.pshs.edu.ph>
2025-02-08 19:58:05 +0800

    Final Changes for q3.js

--- q3.js
+++ q3.js
@@ -5,44 +5,63 @@ var number2;
 function add(number1, number2) { 
     number1 = parseFloat(document.getElementById('num1').value); 
     number2 = parseFloat(document.getElementById('num2').value);
-    const sum = number1 + number2; 
+    const result = sum(number1, number2);  
 
-    document.getElementById("result").innerHTML = "The sum of "+ number1 + " and " + number2 + " is " + sum;
+    document.getElementById("result").innerHTML = "The sum of "+ number1 + " and " + number2 + " is " + result;
 } 
 
 function difference(number1, number2) { 
     number1 = parseFloat(document.getElementById('num1').value); 
     number2 = parseFloat(document.getElementById('num2').value);
-    const diff = number1 - number2; 
+    const result = diff(number1, number2); 
 
-    document.getElementById("result").innerHTML = "The difference of "+ number1 + " and " + number2 + " is " + diff;
+    document.getElementById("result").innerHTML = "The difference of "+ number1 + " and " + number2 + " is " + result;
 } 
 
 function product(number1, number2) { 
     number1 = parseFloat(document.getElementById('num1').value); 
     number2 = parseFloat(document.getElementById('num2').value);
-    const prod = number1 * number2; 
+    const result = prod(number1, number2); 
+    
 
-    document.getElementById("result").innerHTML = "The product of "+ number1 + " and " + number2 + " is " + prod;
+    document.getElementById("result").innerHTML = "The product of "+ number1 + " and " + number2 + " is " + result;
 } 
  
 function quotient(number1, number2) { 
     number1 = parseFloat(document.getElementById('num1').value); 
     number2 = parseFloat(document.getElementById('num2').value);
-    const div = number1 / number2; 
-
+    const result = divide(number1, number2);
+    
+    
     if(number2 > 0 || number2 < 0) { 
-    document.getElementById("result").innerHTML = "The quotient of "+ number1 + " and " + number2 + " is " + div; 
+    document.getElementById("result").innerHTML = "The quotient of "+ number1 + " and " + number2 + " is " + result; 
     } 
     else {
         document.getElementById("result").innerHTML = "I am sorry but 0 is an invalid input value for your second number when it comes to division ( / )";
     }
-    
 } 
  
 function remainder(number1, number2) { 
     number1 = parseFloat(document.getElementById('num1').value); 
     number2 = parseFloat(document.getElementById('num2').value);
-    const rem = number1 % number2; 
-    document.getElementById("result").innerHTML = "The remainder from the quotient of "+ number1 + " and " + number2 + " is " + rem; 
+    const result = rem(number1, number2); 
+    
+    document.getElementById("result").innerHTML = "The remainder from the quotient of "+ number1 + " and " + number2 + " is " + result; 
 } 
+ 
+function sum(x, y) {
+    return x + y;
+} 
+function diff(x, y) {
+    return x - y;
+} 
+function prod(x, y) {
+    return x * y;
+} 
+function divide(x, y) {
+    return x / y;
+} 
+function rem(x, y) {
+    return x % y;
+} 
+
